02ee6f551a6248508a44bf9ae43d723d185464f0
thedrops <pedrops02@gmail.com>
2019-09-02 21:08:22 -0300

    criar e listar usuario

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
@@ -7,6 +9,35 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+var usuarios = [
+  {
+    nome : 'João', 
+    email : 'teste@gmail.com' ,
+    senha : 'teste123',
+  }
+];
+
+app.post('/usuarios/novo', (req, res, next) =>{
+  let novoUsuario = {
+    nome : req.body.nome,
+    email : req.body.email,
+    senha : req.body.senha,
+  }
+  usuarios.push(novoUsuario);
+  res.redirect('/usuarios');
+});
+
+app.get('/usuarios/novo',(req, res, next) =>{
+  res.render('novoUsuario');
+});
+
+app.get('/usuarios', (req, res, next) =>{
+  res.render('usuarios',{
+    'usuarios' : usuarios,
+  })
+});
 
 app.get('/', (req, res, next) => {
   res.render('index', {
