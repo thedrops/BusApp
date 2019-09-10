5001c30940af74fa446fbe5d1b735a2f2c338d88
rebecastilho <becacastilhocamargo@gmail.com>
2019-09-09 22:33:27 -0300

    Editar linha e leitura de arquivos com promises

--- models/LinhaModel.js
+++ models/LinhaModel.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid/v4');
 
 const dbPath = path.join(
     path.dirname(process.mainModule.filename),
@@ -8,34 +9,63 @@ const dbPath = path.join(
 );
 
 class Linha {
-    constructor(origem, destino, tarifa, horarioSaida){
+    constructor(origem, destino, tarifa, horarioSaida,id){
+        if(!id){
+            this.id = uuid();
+        }
+        else{
+            this.id = id;
+        }
         this.origem = origem;
         this.destino = destino;
         this.tarifa = tarifa;
         this.horarioSaida = horarioSaida;
     }
 
-    salvar() {
-        let linhas = [];
-        try{
-            let data = fs.readFileSync(dbPath);
-            linhas = JSON.parse(data.toString()); 
-        }
-        catch(e){}
-
-        linhas.push(this);
-        fs.writeFileSync(dbPath, JSON.stringify(linhas));
+    static getById(id){
+        return lerLinhas()
+            .then((linhas)=>{
+                return linhas.find((element)=>{
+                    return element.id == id;
+                });
+            })
     }
 
-    static listar() {
-        let linhas = [];
-        try{
-            let data = fs.readFileSync(dbPath);
-            linhas = JSON.parse(data.toString()); 
-        }
-        catch(e){}
-        return linhas;
+    salvar() {
+        lerLinhas()
+        .then((linhas)=>{
+            linhas.push(this);
+            fs.writeFileSync(dbPath, JSON.stringify(linhas));
+        })
+        .catch((err) => {
+            console.log(err);
+            let linhas = [this];
+            fs.writeFileSync(dbPath, JSON.stringify(linhas));
+        })
+    }
+    
+    
+    static listar(){
+        return lerLinhas();
     }
 }
+
+    const lerLinhas = () => {
+
+        return new Promise((resolve,reject) => {
+            fs.readFile(dbPath, (err, data) => {
+                if (err) {
+                    return reject(linhas);
+                }
+                let linhas = JSON.parse(data.toString());
+                return resolve(linhas);
+        });
+    });
+    
+
+
+    }
+
+
 
 module.exports = Linha;
